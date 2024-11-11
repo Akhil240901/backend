4f870e5ce20265fbafe99352791e5d7a67aea3b3
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2024-11-11 13:45:44 +0530

    performed asynchronous CRUD OPE

---
+++ index.js
@@ -0,0 +1,21 @@
+//file system core module
+const fs=require("fs");
+
+//writeFileSync("file name","Content");
+      // fs.writeFileSync("read.txt","Hello node");
+// this will override previous content 
+       //fs.writeFileSync("read.txt","Hello node from akhil this side");
+
+//append content in next line
+//fs.appendFileSync("read.txt"," lets go hard");
+
+//read and display content
+const buff_content=fs.readFileSync("read.txt");
+// console.log(buff_content);  //this will display bite code or buffer value
+org_content=buff_content.toString();
+console.log(org_content);
+
+//rename file content
+fs.renameSync("read.txt","read_file.txt")
+
+//delete content
