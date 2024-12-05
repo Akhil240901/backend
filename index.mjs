5474a1b0aa0cd12ace67a74707ad3f81502914f5
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2024-12-05 16:09:34 +0530

    added some highlights

--- index.mjs
+++ index.mjs
@@ -5,9 +5,9 @@ import EventEmitter from "events";
 //create object
 const event=new EventEmitter();
 
-
+//first function defination then 
 event.on('sayMyName',()=>{
                               console.log("My name is akhil");
 })
-
+//then function call ,always have points on it
 event.emit("sayMyName");
