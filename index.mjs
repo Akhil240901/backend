4099a1f6b8d09edee786b3db9c605eef966f03e3
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2024-12-05 16:06:59 +0530

    performed events

--- index.mjs
+++ index.mjs
@@ -1,34 +1,13 @@
-//install http server 
-import http from "http";
+// const EventEmitter=require('events');
+//import EventEmitter as class from event module
+import EventEmitter from "events";
 
-//createserver 
-const server=http.createServer((req,res)=>{
-                              res.writeHead(200, { 'Content-Type': 'text/plain' });
-      if(req.url=="/"){
-                              res.end("hey welcome to home page")
-      }else if(req.url=="/about"){
-                              res.end("welcome to about us page")
-      }else{
-                              res.end("404 Error")
-      }
-});
+//create object
+const event=new EventEmitter();
 
-server.listen(5500,()=>{
-       console.log("Server is running on port 8080")
+
+event.on('sayMyName',()=>{
+                              console.log("My name is akhil");
 })
 
-// import http from 'http';
-
-// // Create a server
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello, ES Modules!\n');
-// });
-
-// // Define the port to listen on
-// const PORT = 3000;
-
-// // Start the server
-// server.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
-// });
+event.emit("sayMyName");
