b1c2538d4d9af809954f5e03513dfd6445f1ce96
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2024-12-08 14:17:44 +0530

    sent json data as response

--- express/index.mjs
+++ express/index.mjs
@@ -16,10 +16,23 @@ app.get("/contact", (req, res) => {
 });
 
 app.get("/temp", (req, res) => {
-  res.send({
+  res.send([{
     id:1,
     name:"Akhil",
-  });
+  },
+  {
+    id:2,
+    name:"Nikhil",
+  },
+  {
+    id:1,
+    name:"sahil",
+  },
+  {
+    id:1,
+    name:"Amol",
+  },
+]);
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
