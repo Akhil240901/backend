3add023ad031f420f9f7842ce28efc41a97521a4
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2024-12-08 14:42:12 +0530

    sent html page as response

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -1,7 +1,17 @@
 import express from "express";
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from "path";
 const app = express();
 
+// Get the __filename equivalent
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the __dirname equivalent
+const __dirname = dirname(__filename);
+
+const staticPath=path.join(__dirname,"../public")
+app.use(express.static(staticPath));
 app.get("/", (req, res) => {
   res.send("<h1> Hello, Express!</h1>");
 });
