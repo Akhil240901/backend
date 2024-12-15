627c1f393e8ed9364f21dce4532fcfb8a1179ac7
akhilbisen <65997834+Akhil240901@users.noreply.github.com>
2024-12-15 13:46:27 +0530

    hbs template and served dynamic content

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from "path";
+import hbs from "hbs";
 const app = express();
 
 // Get the __filename equivalent
@@ -13,8 +14,17 @@ const __dirname = dirname(__filename);
 //absolute or full path
 const staticPath=path.join(__dirname,"../public")
 
+//to set view engine
+app.set("view engine","hbs");
+
+app.get("",(req,res)=>{
+  //.render will serve page and send data just like props in react in object
+  res.render("index.hbs",{
+    sourceName:"Akhil",
+  })
+})
 //using middleware, below line will show html page
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
   res.send("<h1> Hello, Express!</h1>");
